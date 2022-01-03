cd3c915137f65c418ddf532fbfd6bf27795c67f0
nabucodonosor-java <nabucodonosor.java@gmail.com>
2022-01-03 10:01:21 -0300

    12-22 SalesSummary integration ok

--- front-web/src/components/sales-summary/index.tsx
+++ front-web/src/components/sales-summary/index.tsx
@@ -3,15 +3,43 @@ import { ReactComponent as MoneyIcon } from './../../assets/money-icon.svg';
 import { ReactComponent as AmountIcon } from './../../assets/amount-icon.svg';
 import { ReactComponent as MinimaIcon } from './../../assets/minima-icon.svg';
 import { ReactComponent as MaximaIcon } from './../../assets/maxima-icon.svg';
+import { FilterData, SalesSummaryData } from '../../types';
 import './styles.css';
+import { useEffect, useMemo, useState } from 'react';
+import { buildFiltersParams, makeRequest } from '../../utils/request';
 
-const SalesSummary = () => {
+type Props = {
+  filterData?: FilterData;
+};
+
+const initalSummary = {
+  avg: 0,
+  count: 0,
+  max: 0,
+  min: 0
+};
+
+const SalesSummary = ({ filterData }: Props) => {
+  const [summary, setSummary] = useState<SalesSummaryData>(initalSummary);
+  const params = useMemo(() => buildFiltersParams(filterData), [filterData]);
+
+  useEffect(() => {
+    makeRequest
+      .get<SalesSummaryData>('/sales/summary', { params })
+      .then((response) => {
+        setSummary(response.data);
+      })
+      .catch(() => {
+        console.error('Erro de integração coma API');
+      });
+  }, [params]);
+
   return (
     <div className="sales-summary-container">
-      <SalesSummaryCard value={351.0} label="Média" icon={<MoneyIcon />} />
-      <SalesSummaryCard value={56.0} label="Quantidade" icon={<AmountIcon />} />
-      <SalesSummaryCard value={51.0} label="Mínima" icon={<MinimaIcon />} />
-      <SalesSummaryCard value={31.0} label="Máxima" icon={<MaximaIcon />} />
+      <SalesSummaryCard value={summary?.avg?.toFixed(2)} label="Média" icon={<MoneyIcon />} />
+      <SalesSummaryCard value={summary?.count} label="Quantidade" icon={<AmountIcon />} />
+      <SalesSummaryCard value={summary?.min} label="Mínima" icon={<MinimaIcon />} />
+      <SalesSummaryCard value={summary?.max} label="Máxima" icon={<MaximaIcon />} />
     </div>
   );
 };
